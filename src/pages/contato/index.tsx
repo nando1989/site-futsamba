1e08c4301f2ba6eaf2d79ffdee279deb1debf873
gelaco1989 <gelaco1989@gmail.com>
2024-07-25 21:31:28 -0300

    concertando erro de deploy em "contato"

--- src/pages/contato/index.tsx
+++ src/pages/contato/index.tsx
@@ -1,7 +1,5 @@
 /* eslint-disable jsx-a11y/alt-text */
-import React from 'react'
-import Futsamba2 from './images/futsamba-99.png'
-import Burger from '../../components/burger'
+
 import './styles.css'
 import Whatsapp from './images/Whatsapp.png'
 import Facebook from './images/facebook.png'
@@ -13,7 +11,7 @@ const Contato = () => {
     <div>
       <div className='container-contact'>
         <div className='date'>
-          
+
           <div className='icons'>
             <div className='icon-contato'> <a href="https://www.linkedin.com/in/fernando-de-souza-84b0652a6/"> <img className='whatsapp' src={Whatsapp} alt="Whatsapp" /> </a></div>
             <div className='icon-contato'> <a href="https://www.linkedin.com/in/fernando-de-souza-84b0652a6/"> <img className='whatsapp' src={Facebook} alt="Whatsapp" /> </a></div>
